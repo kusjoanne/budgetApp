defe9d83d98631bb8011bb58b9ddc98a411935da
Joanne Kus <kusjoanne@gmail.com>
2020-05-20 16:13:13 -0500

    updated react-bootstrap modals

--- client/src/AddItemModal.js
+++ client/src/AddItemModal.js
@@ -4,28 +4,39 @@ import dateService from "./services/dateService";
 import "react-datepicker/dist/react-datepicker.css";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function AddItemModal(props){
   const [itemName, setItemName] = useState('');
   const [itemAmount, setItemAmount] = useState('');
   const [startDate, setStartDate] = useState(new Date());
+  const [validated, setValidated] = useState(false);
+
 
   function handleAdd(event){
     event.preventDefault();
-    let myForm = {};
-    let formData = new FormData(event.target);
-    formData.forEach(function(value, key){
-        myForm[key] = value;
-    });
+    setValidated(true);
+    const form = event.currentTarget;
+    if (form.checkValidity() === true) {
+      event.preventDefault();
+      let myForm = {};
+      let formData = new FormData(event.target);
+      formData.forEach(function(value, key){
+          myForm[key] = value;
+      });
 
-    const addItem = async ()=>{
-      await dateService.add(myForm);
-      setItemName('');
-      setItemAmount('');
-      props.getBalance();
-      props.refreshItemDates();
+      const addItem = async ()=>{
+        event.preventDefault();
+        await dateService.add(myForm);
+        setItemName('');
+        setItemAmount('');
+        props.getBalance();
+        props.refreshItemDates();
+        props.onHide();
+        setValidated(false);
+      }
+      addItem();
     }
-    addItem();
   }
 
   return <Modal show={props.show} onHide={props.onHide} animation={true} centered>
@@ -33,26 +44,38 @@ function AddItemModal(props){
       <Modal.Title>ADD ITEM</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <form className=""  id="addForm" onSubmit={handleAdd}>
-        <div id="item-name">
-          <label htmlFor="itemName"><strong>Item Name</strong></label>
-          <input type="text" name="itemName" value={itemName} className="itemName" onChange={itemName => setItemName(itemName.target.value)}/>
-        </div>
-        <div id="item-amount">
-          <label htmlFor="itemAmount"><strong>Amount</strong></label>
-          <input type="text" name="itemAmount" value={itemAmount} className="itemAmount" onChange={itemAmount => setItemAmount(itemAmount.target.value)} />
-        </div>
-        <div id="item-date">
-          <label htmlFor="itemDate"><strong>Date</strong></label>
-          <DatePicker name="itemDate" selected={startDate} onChange={date => setStartDate(date)} />
-        </div>
-      </form>
+      <Form noValidate validated={validated} id="addForm" onSubmit={handleAdd}>
+        <Form.Group controlId="formItemName">
+          <Form.Label>Item Name</Form.Label>
+          <Form.Control name="itemName" type="text" placeholder="Enter Item Name" required/>
+          <Form.Control.Feedback type="invalid">
+            Please provide an item name.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="formItemAmount">
+          <Form.Label>Amount</Form.Label>
+          <Form.Control name="itemAmount" type="text" placeholder="Enter Amount" required/>
+          <Form.Control.Feedback type="invalid">
+            Please provide an item amount.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="formItemDate">
+          <Form.Label>Item Date</Form.Label>
+          <br></br>
+          <DatePicker name="itemDate" selected={startDate} onChange={date => setStartDate(date)} required/>
+          <Form.Control.Feedback type="invalid">
+            Please provide an item date.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Form>
     </Modal.Body>
     <Modal.Footer>
       <Button variant="secondary" onClick={props.onHide}>
         Close
       </Button>
-      <Button variant="primary" onClick={props.onHide} type="submit" id="addItem" form="addForm">
+      <Button variant="primary" type="submit" id="addItem" form="addForm">
         Save Changes
       </Button>
     </Modal.Footer>
--- client/src/EditItemModal.js
+++ client/src/EditItemModal.js
@@ -4,28 +4,36 @@ import "react-datepicker/dist/react-datepicker.css";
 import dateService from "./services/dateService";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function EditItemModal(props) {
   const [itemName, setItemName] = useState(props.itemName);
   const [itemAmount, setItemAmount] = useState(props.itemAmount);
   const [itemDate, setItemDate] = useState(new Date(props.itemDate));
+  const [validated, setValidated] = useState(false);
 
   const handleEdit = (event) => {
     event.preventDefault();
-    let myForm = {
-      id: props.id,
-      originalDate: props.itemDate,
-      originalAmount: props.itemAmount
-    };
-    let formData = new FormData(event.target);
-    formData.forEach(function(value, key) {
-      myForm[key] = value;
-    });
-    const editItem = async () => {
-      await dateService.edit(myForm);
-      props.refreshItemDates();
+    setValidated(true);
+    const form = event.currentTarget;
+    if (form.checkValidity() === true) {
+      event.preventDefault();
+      let myForm = {
+        id: props.id,
+        originalDate: props.itemDate,
+        originalAmount: props.itemAmount
+      };
+      let formData = new FormData(event.target);
+      formData.forEach(function(value, key) {
+        myForm[key] = value;
+      });
+      const editItem = async () => {
+        await dateService.edit(myForm);
+        props.refreshItemDates();
+        props.onHide();
+      }
+      editItem();
     }
-    editItem();
   }
 
   const handleDelete = (event) => {
@@ -45,33 +53,39 @@ function EditItemModal(props) {
       <Modal.Title>ADD ITEM</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <form onSubmit={handleEdit} id={"editForm" + props.id}>
-        <div id="item-name">
-          <label htmlFor="itemName">
-            <strong>Item Name</strong>
-          </label>
-          <input type="text" name="itemName" value={itemName} className="itemName" onChange={itemName => setItemName(itemName.target.value)}/>
-        </div>
-        <div id="item-amount">
-          <label htmlFor="itemAmount">
-            <strong>Amount</strong>
-          </label>
-          <input type="text" name="itemAmount" value={itemAmount} className="itemAmount" onChange={itemAmount => setItemAmount(itemAmount.target.value)}/>
-        </div>
-        <div id="item-date">
-          <label htmlFor="itemDate">
-            <strong>Date</strong>
-          </label>
-          <DatePicker name="itemDate" selected={itemDate} onChange={date => setItemDate(date)}/>
-        </div>
-      </form>
+      <Form noValidate validated={validated} onSubmit={handleEdit} id={"editForm" + props.id}>
+        <Form.Group controlId="formItemName">
+          <Form.Label>Item Name</Form.Label>
+          <Form.Control name="itemName"  type="text" value={itemName} className="itemName" onChange={itemName => setItemName(itemName.target.value)} required/>
+          <Form.Control.Feedback type="invalid">
+            Please provide an item name.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="formItemAmount">
+          <Form.Label>Amount</Form.Label>
+          <Form.Control name="itemAmount" type="text" value={itemAmount} className="itemAmount" onChange={itemAmount => setItemAmount(itemAmount.target.value)}/>
+          <Form.Control.Feedback type="invalid">
+            Please provide an item amount.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group controlId="formItemDate">
+          <Form.Label>Item Date</Form.Label>
+          <br></br>
+          <DatePicker name="itemDate" selected={itemDate} onChange={date => setItemDate(date)} />
+          <Form.Control.Feedback type="invalid">
+            Please provide an item date.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Form>
     </Modal.Body>
     <Modal.Footer>
       <Button variant="secondary" onClick={props.onHide}>Close</Button>
       <form onSubmit={handleDelete} id={"deleteForm" + props.id}>
         <Button variant="danger" onClick={props.onHide} type="submit" id="deleteItem">Delete Item</Button>
       </form>
-      <Button variant="primary" type="submit" id="editItem" onClick={props.onHide} form={"editForm" + props.id}>Save Changes</Button>
+      <Button variant="primary" type="submit" id="editItem" form={"editForm" + props.id}>Save Changes</Button>
     </Modal.Footer>
   </Modal>
 
